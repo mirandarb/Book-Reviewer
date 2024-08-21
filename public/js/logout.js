198b554a59bea2baec6a58a80a30fa98b6b2c392
Kent Ball <kent.vincent.ball@gmail.com>
2024-08-20 20:45:58 -0400

    Fixed user logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,14 @@
-// Manages user logout
-// Handle logout
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/'); // Redirect to homepage after logout
-    } else {
-      alert('Failed to log out.');
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out.');
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
